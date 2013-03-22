05a80af6b104fed60e25f47e54d121c3e9c62a02
teo <teo.sibileau@gmail.com>
2013-03-22 11:06:37 -0300

    some refactoring done. access keys stored in user's home folder instead of python file

--- dropboxcli/dropboxcli.py
+++ dropboxcli/dropboxcli.py
@@ -1,15 +1,79 @@
 #!/usr/bin/env python
 import os
 import sys
-import settings
-from dropbox import client, rest, session
+import yaml
+from dropbox import client, session
+
+from termcolor import cprint
 
 SCRIPT_ROOT = os.path.dirname(os.path.realpath(__file__))
-TOKEN_FILE = os.path.join(SCRIPT_ROOT,'.access_token')
+TOKEN_FILE = os.path.join(SCRIPT_ROOT, '.access_token')
+
+LINE_LENGHT = 40
+
+
+class Header(object):
+    def __init__(self, text, color='white'):
+        print '\n'
+        CPRINT = lambda x: cprint(x, color)
+        line = ''.join(['#' for i in range(LINE_LENGHT)])
+        CPRINT(line)
+        CPRINT('\t %s' % text)
+        CPRINT(line)
+        print '\n'
+
+
+class SimpleLine(object):
+    def __init__(self, text, color='white'):
+        CPRINT = lambda x: cprint(x, color)
+        CPRINT(text)
+
+YAML = os.path.join(
+    os.path.expanduser('~'),
+    '.dropboxcli.yaml'
+)
+
+
+class Access(object):
+    app_key = None
+    app_secret = None
+    access_type = 'app_folder'
+    consolidated = False
+
+    def __init__(self):
+        try:
+            f = file(YAML, mode='r')
+            y = yaml.load(f)
+            SimpleLine('Accessing yaml settings', 'cyan')
+            self.app_key = y['access']['app_key']
+            self.app_secret = y['access']['app_secret']
+            self.secret = y['access']['access_type']
+            f.close()
+            self.consolidated = True
+        except:
+            SimpleLine('cant find YAML settings, generating one', 'magenta')
+            self.app_key = raw_input("Please enter your Dropbox APP_KEY: ")
+            self.app_secret = raw_input("Please enter your Dropbox APP_SECRET: ")
+
+        if not self.consolidated:
+            f = file(YAML, mode='w+')
+            y = {}
+            y['access'] = {}
+            y['access']['app_key'] = self.app_key
+            y['access']['app_secret'] = self.app_secret
+            y['access']['access_type'] = self.access_type
+            yaml.dump(y, f, indent=4)
+            f.close()
+
 
 class Auth():
     def __init__(self):
-        self.sess = session.DropboxSession(settings.APP_KEY, settings.APP_SECRET, settings.ACCESS_TYPE)
+        self.access = Access()
+        self.sess = session.DropboxSession(
+            self.access.app_key,
+            self.access.app_secret,
+            self.access.access_type
+        )
         try:
             with open(TOKEN_FILE, 'r') as token_file:
                 for line in token_file:
@@ -30,17 +94,21 @@ class Auth():
                 token_file.close()
             self.client = client.DropboxClient(self.sess)
 
+
 def upload():
+    Header('Dropbox Uploader', 'cyan')
     args = sys.argv
     args.pop(0)
     path = os.getcwd()
     a = Auth()
     for arg in args:
         try:
-            f = open(os.path.join(path,arg))
-            response = a.client.put_file(arg,f)
+            f = open(os.path.join(path, arg))
+            response = a.client.put_file(arg, f)
             print "uploaded", response
         except Exception as e:
             print e
 
-upload()
+
+if __name__ == '__main__':
+    upload()
--- dropboxcli/settings.py
+++
@@ -1,3 +0,0 @@
-APP_KEY = 'qjhzdcne90p8iv0'
-APP_SECRET = 'ulvi9170juarrsp'
-ACCESS_TYPE = 'app_folder'
